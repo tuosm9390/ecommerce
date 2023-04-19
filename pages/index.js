8702a88d0683154f84268e2039307c1667f450c8
tuosm9390 <tuosm123@naver.com>
2023-04-19 17:17:19 +0900

    init commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Product, FooterBanner, HeroBanner } from "../components";
+
+const Home = () => {
+  return (
+    <>
+      <HeroBanner />
+
+      <div className="products-heading">
+        <h2>Beset Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div className="products-container">
+        {["product 1", "product 2"].map((product) => product)}
+      </div>
+
+      <FooterBanner />
+    </>
+  );
+};
+
+export default Home;
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kalm759a',
+    dataset: 'production'
+  }
+})
